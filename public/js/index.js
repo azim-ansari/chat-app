b139cac68f2f95edc2e4635bcae4b417a88c100a
Azim Ansari <azimpanjwar@gmail.com>
2021-07-29 11:55:26 +0530

    broadcasting added

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 let socket = io();
 socket.on("connect", () => {
 	console.log("connected to server.");
-
-	socket.emit("createMessage", {
-		from: "Azim",
-		text: "Whats going on !!",
-	});
 });
 socket.on("disconnect", () => {
 	console.log("diconnected from server.");
